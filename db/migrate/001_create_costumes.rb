a0950f1f1ed824cd304793a8e60469f2f4abc18a
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-14 04:41:52 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,5 +8,13 @@
 # In this change method, create columns with the correct names and
 # value types according to the spec
 class CreateCostumes < ActiveRecord::Migration[4.2]
-
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :price
+      t.string :image_url
+      t.size :size
+      t.timestamps
+    end
+  end
 end
