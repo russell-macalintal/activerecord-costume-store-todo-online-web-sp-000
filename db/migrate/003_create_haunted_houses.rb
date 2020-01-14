7ff93d9f40882ec62a91daf69659ef9ea1c8ed5d
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-14 05:11:03 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,8 +7,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.string :opening_date
-      t.string :closing_date
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.text :description
     end
   end
